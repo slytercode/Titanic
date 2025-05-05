f31e490cdbd80c6c07575f9db7fcf651ea4ca079
Simone <18simonepiantoni@gmail.com>
2025-05-05 13:39:29 +0200

    Cleaned the dataset reading section and handled missing values

--- src/Titanic.py
+++ src/Titanic.py
@@ -2,9 +2,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 df = pd.read_csv("C:\\Users\\pc\\Desktop\\PROJECTS\\Titanic\\data\\train.csv")
-print(df.head)
-print(df.shape)
-print(df.columns)
-print(df.info())
-print(df.describe)
-print(df.isnull().sum())
+
+
+df.drop(columns="Cabin", inplace=True)
+df["Age"].fillna(df["Age"].mean(), inplace=True)
+df= df[df["Embarked"].notnull()]
