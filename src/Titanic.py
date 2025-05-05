05dce274f833f3bc3a9eed4891b7a99e9b2123f9
Simone <18simonepiantoni@gmail.com>
2025-05-05 14:08:13 +0200

    Added exploratory print statements for grouped statistics (Pclass, Age, Survived, Sex)

--- src/Titanic.py
+++ src/Titanic.py
@@ -3,7 +3,18 @@ import matplotlib.pyplot as plt
 
 df = pd.read_csv("C:\\Users\\pc\\Desktop\\PROJECTS\\Titanic\\data\\train.csv")
 
-
+#clean NaN values
 df.drop(columns="Cabin", inplace=True)
 df["Age"].fillna(df["Age"].mean(), inplace=True)
 df= df[df["Embarked"].notnull()]
+correlation = df.corr(numeric_only=True)
+print (correlation["Survived"].sort_values(ascending=False))
+
+# "Pclass" is a non-human attribute with strong correlation to survival
+
+print(df["Pclass"].value_counts())
+print(df.groupby("Pclass")["Age"].mean())
+print(df.groupby("Survived")["Sex"].count())
+print(df.groupby("Sex")["Survived"].mean())
+print(df.groupby(["Pclass", "Sex", "Survived"]).size())
+print(df.groupby(["Pclass", "Sex"])["Survived"].mean() * 100)
